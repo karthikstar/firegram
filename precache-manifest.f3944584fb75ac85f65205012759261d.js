8c4ce701bfcf332361b855fa3595a1ebae0117b6
elkarthi2000@gmail.com <elkarthi2000@gmail.com>
2020-08-15 23:06:07 +0800

    Updates

---
+++ precache-manifest.f3944584fb75ac85f65205012759261d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c46568c9f5e5cda5ffafc7f7e24c423",
+    "url": "/firegram/index.html"
+  },
+  {
+    "revision": "6d6002812888f12dc980",
+    "url": "/firegram/static/css/main.e3bd3a6e.chunk.css"
+  },
+  {
+    "revision": "c5f2121596d8fa3371df",
+    "url": "/firegram/static/js/2.940794cb.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/firegram/static/js/2.940794cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d6002812888f12dc980",
+    "url": "/firegram/static/js/main.fe4bb18b.chunk.js"
+  },
+  {
+    "revision": "a20b20e63cdcd0ca9e81",
+    "url": "/firegram/static/js/runtime-main.9968160c.js"
+  }
+]);
